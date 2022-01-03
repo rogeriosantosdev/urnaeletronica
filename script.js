c37f03591fe841907bfdb3e2358078ca38eacdea
Rogério <rogeriosantosdev@gmail.com>
2022-01-03 17:06:44 -0300

    implementado corrige, branco e confirma

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
 let numero = '';
+let votoBranco = false;
 
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual];
 
     let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
 
     for(let i = 0; i < etapa.numeros; i++){
         if(i===0){
@@ -48,9 +51,14 @@ function atualizaInterface(){
         let fotosHtml = '';
 
         for(let i in candidato.fotos){
-            fotosHtml += `<div class="d-1-image"> <img src="/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`
-        }
+            if(candidato.fotos[i].small){
+                fotosHtml += `<div class="d-1-image small"> <img src="/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`
+            } else {
 
+                fotosHtml += `<div class="d-1-image"> <img src="/images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`
+            }
+        }
+            
         lateral.innerHTML = fotosHtml;
     } else {
         seuVotoPara.style.display = 'block';
@@ -76,13 +84,40 @@ function clicou(n){
 }
 
 function branco(){
-    alert('clicou em BRANCO')
+    if(numero === ''){
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class="aviso--grande pisca ">VOTO EM BRANCO</div>';
+    } else {
+        alert("Para votar em BRANCO, deixe os espaços vazios!")
+    }
 }
 function corrige(){
-    alert('clicou em CORRIGE')
+    comecarEtapa();
 }
 function confirma(){
-    alert('clicou em CONFIRMA')
+    let etapa = etapas[etapaAtual]
+    
+    let votoConfirmado = false;
+
+    if(votoBranco === true){
+        votoConfirmado = true;
+        console.log('confirmando como branco');
+    } else if (numero.length === etapa.numeros){
+        votoConfirmado = true;
+        console.log("confirmando como: " +numero)
+    }
+
+    if(votoConfirmado){
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa();
+        } else {
+            console.log('fim')
+        }
+    }
 }
 
 comecarEtapa();
